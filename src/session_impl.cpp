280d6a44e723406273378bae5673716f7aad4974
Phillip Engel <phillip@pockethost.app>
2025-11-16 01:55:21 +0000

    bump session log tag

--- src/session_impl.cpp
+++ src/session_impl.cpp
@@ -4486,7 +4486,7 @@ namespace {
 		{
 			session_log("RECALCULATE UNCHOKE SLOTS: [ peers: %d "
 				"eligible-peers: %d"
-				" allowed-slots: %d ] libtorrent_xr 2"
+				" allowed-slots: %d ] libtorrent_xr 3"
 				, int(m_connections.size())
 				, int(peers.size())
 				, allowed_upload_slots);
